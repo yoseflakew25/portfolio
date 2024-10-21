9a22b4c79da33beed4cb9eb64aec0dd30f239613
yosef <jolarohe@gmail.com>
2024-10-21 04:23:38 +0300

    Update InfiniteCards.tsx

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -14,6 +14,7 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
+    thumbnail: string; 
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
